ef8a37ac0529aa3540ea17932d4ae497207630b8
moyuLua <2633699957@qq.com>
2024-03-22 22:07:50 +0800

    2024/03/22

---
+++ coursera/week02/lab.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Files.lab_utils_multi import  load_house_data, run_gradient_descent
+from Files.lab_utils_multi import  norm_plot, plt_equal_scale, plot_cost_i_w
+from Files.lab_utils_common import dlc
+np.set_printoptions(precision=2)
+plt.style.use('Files.deeplearning.mplstyle')
